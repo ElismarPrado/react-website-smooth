ace7922c5fc931c8586a19b9a95beb92c5913631
Elismar Prado <elismar-prado@hotmail.com>
2022-09-14 10:22:22 -0300

    menu work perfectly

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -13,13 +13,13 @@ import {
   NavBtnLink 
 } from './NavbarElements';
 
-const Navbar = () => {
+const Navbar = ({ toggle }) => {
   return (
     <>
       <Nav>
         <NavbarContainer>
           <NavLogo to="/">Dollar</NavLogo>
-          <MobileIcon>
+          <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
           <NavMenu>
--- src/components/Sidebar/SidebarElements.js
+++ src/components/Sidebar/SidebarElements.js
@@ -14,8 +14,8 @@ export const SidebarContainer = styled.aside`
     top: 0;
     left: 0;
     transition: 0.3s ease-in-out;
-    /* opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
-    top: ${({isOpen}) => (isOpen ? '0' : '-100%')}; */
+    opacity: ${({ isOpen }) => (isOpen ? '100%' : '0')};
+    top: ${({isOpen}) => (isOpen ? '0' : '-100%')};
     z-index: 999;
 `
 
@@ -37,7 +37,18 @@ export const SidebarWrapper = styled.div`
     color: #fff;
 `
 
-export const sidebarLink = styled(LinkS)`
+export const SidebarMenu = styled.ul`
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(6, 80px);
+    text-align: center;
+
+    @media screen and (max-width: 480px) {
+        grid-template-rows: repeat(6, 60px);
+    }
+`
+
+export const SidebarLink = styled(LinkS)`
     display: flex;
     align-items: center;
     justify-content: center;
@@ -77,6 +88,5 @@ export const SidebarRoute = styled(LinkR)`
         transition: all 0.2s ease-in-out;
         background: #fff;
         color: #010606;
-
     }
 `
